8cb43f9428a9f4deb63124d655d414b6646e80a5
refrshrs <nthorgan@live.co.uk>
2021-12-24 18:46:58 +0000

    Changes to be committed: 	new file:   data/dejavu16x16_gs_tc.png 	new file:   game/__init__.py 	new file:   game/__pycache__/__init__.cpython-39.pyc 	new file:   game/__pycache__/actions.cpython-39.pyc 	new file:   game/__pycache__/engine.cpython-39.pyc 	new file:   game/__pycache__/entity.cpython-39.pyc 	new file:   game/__pycache__/game_map.cpython-39.pyc 	new file:   game/__pycache__/input_handlers.cpython-39.pyc 	new file:   game/__pycache__/procgen.cpython-39.pyc 	new file:   game/__pycache__/rendering.cpython-39.pyc 	new file:   game/actions.py 	new file:   game/engine.py 	new file:   game/entity.py 	new file:   game/game_map.py 	new file:   game/input_handlers.py 	new file:   game/procgen.py 	new file:   game/rendering.py 	new file:   main.py

---
+++ game/engine.py
@@ -0,0 +1,13 @@
+# /game/engine.py
+from __future__ import annotations
+
+import random
+
+import game.entity
+import game.game_map
+
+
+class Engine:
+    game_map: game.game_map.GameMap
+    player: game.entity.Entity
+    rng: random.Random
