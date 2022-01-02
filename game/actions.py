381c3e5cc802130504bc42a2bffce9695d7e2b93
refrshrs <nthorgan@live.co.uk>
2022-01-02 23:04:50 +0000

    changed actions

--- game/actions.py
+++ game/actions.py
@@ -1,14 +1,22 @@
 # /game/actions.py
 from __future__ import annotations
+from typing import Optional, Tuple, TYPE_CHECKING
 
-import game.entity
+
+if TYPE_CHECKING:
+    import game.engine
+    import game.entity
 
 
 class Action:
     def __init__(self, entity: game.entity.Entity) -> None:
         super().__init__()
         self.entity = entity  # The object performing the action.
-        self.engine = entity.gamemap.engine
+    
+    @property
+    def engine(self) -> game.engine.Engine:
+        """Returns the engine this action belongs to"""
+        return self.entity.gamemap.engine
 
     def perform(self) -> None:
         """Perform this action now.
@@ -17,23 +25,34 @@ class Action:
         """
         raise NotImplementedError()
 
+class EscapeAction(Action):
+    def perform(self) -> None:
+        raise SystemExit
+
 
 class ActionWithDirection(Action):
     def __init__(self, entity: game.entity.Entity, dx: int, dy: int):
         super().__init__(entity)
-
         self.dx = dx
         self.dy = dy
+
+    @property
+    def dest_xy(self) -> Tuple[int, int]:
+        """Returns this actions destination"""
+        return self.entity.x + self.dx, self.entity.y + self.dy
+
+    @property
+    def blocking_entity(self) -> Optional[game.entity.Entity]:
+        """Returns the blocking entity at this actions destination"""
+        return self.engine.game_map.get_blocking_entity_at(*self.dest_xy)
 
     def perform(self) -> None:
         raise NotImplementedError
 
 
 class Move(ActionWithDirection):
-
     def perform(self) -> None:
-        dest_x = self.entity.x + self.dx
-        dest_y = self.entity.y + self.dy
+        dest_x, dest_y = self.dest_xy
 
         if not self.engine.game_map.in_bounds(dest_x, dest_y):
             return  # Destination is out of bounds.
@@ -43,23 +62,14 @@ class Move(ActionWithDirection):
         self.entity.x, self.entity.y = dest_x, dest_y
 
 class Melee(ActionWithDirection):
-
     def perform(self) -> None:
-        dest_x = self.entity.x + self.dx
-        dest_y = self.entity.y + self.dy
-        target = self.engine.game_map.get_blocking_entity_at(dest_x, dest_y)
-        if not target:
-            return  # No entity to attack.
+        target = self.blocking_entity
         print(f"You kick the {target.name}, much to its annoyance!")
 
 
 class Bump(ActionWithDirection):
-
     def perform(self) -> None:
-        dest_x = self.entity.x + self.dx
-        dest_y = self.entity.y + self.dy
-
-        if self.engine.game_map.get_blocking_entity_at(dest_x, dest_y):
+        if self.blocking_entity:
             return Melee(self.entity, self.dx, self.dy).perform()
         else:
             return Move(self.entity, self.dx, self.dy).perform()
